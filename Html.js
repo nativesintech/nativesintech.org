48d7590411de428ccffac98c4e511d221626bbe3
Adam Recvlohe <adam.recvlohe@gmail.com>
2018-12-30 17:39:13 -0500

    Fix invalid react prop charset 🐛

--- Html.js
+++ Html.js
@@ -8,7 +8,7 @@ export default ({ App, render }) => {
   return (
     <html>
       <head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
