879abc85dc26a69a77f7898c5ab4d83963c58ebd
Adam Recvlohe <adam.recvlohe@gmail.com>
2018-12-30 22:32:39 -0500

    Create global styles css file

--- App.js
+++ App.js
@@ -4,11 +4,13 @@ import * as React from "react";
 import { Router, Route, browserHistory } from "react-router";
 import { createApp, renderApp } from "@phenomic/preset-react-app/lib/client";
 import { withPhenomicApi } from "@phenomic/preset-react-app/lib/es6/src/phenomicPresetReactApp.bs.js";
-import "normalize.css";
 
 import * as Home from "./lib/es6/src/components/Home.bs.js";
 import * as Post from "./lib/es6/src/components/Post.bs.js";
 import ErrorPage from "./lib/es6/src/components/ErrorPage.bs.js";
+
+import "normalize.css";
+import "./global.css";
 
 const routes = () => (
   <Router history={browserHistory}>
