a3359a43b0a36d52affb4f163fe2ec2754d4e20b
Adam Recvlohe <adam.recvlohe@gmail.com>
2019-01-20 20:43:11 -0500

    Add favicon stuff

--- Html.js
+++ Html.js
@@ -49,9 +49,31 @@ export default ({ App, render }) => {
           property="og:image"
           content="https://avatars2.githubusercontent.com/u/35468375?v=4"
         />
+        {/* Favicon */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#7b7b7b" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        {/* Styles */}
         <link rel="stylesheet" href="/styles.css" />
         <style dangerouslySetInnerHTML={{ __html: css }} />
-        <script src="https://www.google.com/recaptcha/api.js" />
       </head>
       <body>
         <Main />
