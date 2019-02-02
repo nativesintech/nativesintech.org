d54caeabd92b2d2581e7daa6b28e606dfc806a19
Adam Recvlohe <adam.recvlohe@gmail.com>
2019-02-01 21:46:59 -0500

    Put SEO back in the Html.js file

--- Html.js
+++ Html.js
@@ -8,6 +8,7 @@ export default ({ App, render }) => {
   return (
     <html>
       <head>
+        <title>Natives in Tech</title>
         <meta charSet="utf-8" />
         <meta
           name="google-site-verification"
@@ -16,6 +17,38 @@ export default ({ App, render }) => {
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta
+          name="keywords"
+          content="natives in tech, natives, indigenous, tech, software development, open source"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@nativesintech" />
+        <meta name="twitter:creator" content="@nativesintech" />
+        <meta name="twitter:title" content="Natives in Tech" />
+        <meta
+          name="twitter:description"
+          content="Natives in Tech is a coalition of Native and non-Native developers who seek
+          to empower and support Native communities around the world through software development."
+        />
+        <meta
+          name="twitter:image"
+          content="https://nativesintech.org/images/og-image.jpg"
+        />
+        <meta property="og:site_name" content="Natives in Tech" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image:height" content="319" />
+        <meta property="og:image:width" content="609" />
+        <meta property="og:title" content="Natives in Tech" />
+        <meta
+          property="og:description"
+          content="Natives in Tech is a coalition of Native and non-Native developers who seek
+          to empower and support Native communities around the world through software development."
+        />
+        <meta property="og:url" content="http://nativesintech.org" />
+        <meta
+          property="og:image"
+          content="https://nativesintech.org/images/og-image.jpg"
         />
         {/* Favicon */}
         <link
