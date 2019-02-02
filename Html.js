d0333d1aea6de4a7ee75524f3b87295cc2cfca82
Adam Recvlohe <adam.recvlohe@gmail.com>
2019-02-01 21:52:19 -0500

    Use react-helmet to render meta properly

--- Html.js
+++ Html.js
@@ -1,14 +1,16 @@
 import React from "react";
+import Head from "react-helmet";
 import { extractCritical } from "emotion-server";
 
 export default ({ App, render }) => {
   const { html, Main, State, Script } = render(<App />);
   const { css, ids } = extractCritical(html);
 
+  const helmet = Head.renderStatic();
+
   return (
-    <html>
+    <html {...helmet.htmlAttributes.toComponent()}>
       <head>
-        <title>Natives in Tech</title>
         <meta charSet="utf-8" />
         <meta
           name="google-site-verification"
@@ -18,39 +20,16 @@ export default ({ App, render }) => {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <meta
-          name="keywords"
-          content="natives in tech, natives, indigenous, tech, software development, open source"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@nativesintech" />
-        <meta name="twitter:creator" content="@nativesintech" />
-        <meta name="twitter:title" content="Natives in Tech" />
-        <meta
-          name="twitter:description"
-          content="Natives in Tech is a coalition of Native and non-Native developers who seek
-          to empower and support Native communities around the world through software development."
-        />
-        <meta
-          name="twitter:image"
-          content="https://nativesintech.org/images/og-image.jpg"
-        />
-        <meta property="og:site_name" content="Natives in Tech" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image:height" content="319" />
-        <meta property="og:image:width" content="609" />
-        <meta property="og:title" content="Natives in Tech" />
-        <meta
-          property="og:description"
-          content="Natives in Tech is a coalition of Native and non-Native developers who seek
-          to empower and support Native communities around the world through software development."
-        />
-        <meta property="og:url" content="http://nativesintech.org" />
-        <meta
-          property="og:image"
-          content="https://nativesintech.org/images/og-image.jpg"
-        />
-        {/* Favicon */}
+
+        {helmet.meta.toComponent()}
+        {helmet.title.toComponent()}
+        {helmet.base.toComponent()}
+
+        {helmet.link.toComponent()}
+        {helmet.style.toComponent()}
+        {helmet.script.toComponent()}
+        {helmet.noscript.toComponent()}
+
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -76,7 +55,7 @@ export default ({ App, render }) => {
         <link rel="stylesheet" href="/styles.css" />
         <style dangerouslySetInnerHTML={{ __html: css }} />
       </head>
-      <body>
+      <body {...helmet.bodyAttributes.toComponent()}>
         <Main />
         <State />
         <Script />
