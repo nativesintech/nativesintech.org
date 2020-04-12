937c398e5b54a4f7e161fa8d7c879f360cc107d5
Adam Recvlohe <adam.recvlohe@gmail.com>
2020-04-12 14:52:58 -0500

    feat: add typing animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,37 @@
+import Typed from "typed.js";
 import { Layout } from "../components/Layout";
 
-export default function IndexPage() {
+  const typedRef = React.createRef<HTMLDivElement>();
+
+  const options = {
+    strings: [
+      "s@^gaêl", // Euchee/Yuchi
+      "Hesci", // Muscogee (Creek) / Seminole
+      "Yá'át'ééh", // Navajo
+      "Halito", // Choctaw
+      "ᎣᏏᏲ", // Cherokee
+      "Aaniin", // Anishinaabemowin
+      "Wáa sá i yatee", // Tlingit
+      "Pablan", // Inupiaq
+      "Sekoh", // Mohawk / Kanien'keha
+      "Howa", // Osage
+      "Ba'ax ka wa'alik", // Mayan
+      "Niltse", // Nahuatl
+      "Tanisi", // Cree
+      "Hello", // English
+    ].map((s) => s + "!"),
+    typeSpeed: 70,
+    backSpeed: 50,
+    loop: true,
+    backDelay: 500,
+  };
+
+  React.useEffect(() => {
+    if (typedRef.current) {
+      new Typed(".typed", options);
+    }
+  }, [typedRef.current]);
+
   return (
     <Layout>
       <div className="flex flex-col lg:flex-row items-center justify-center mx-8">
@@ -9,6 +40,10 @@ export default function IndexPage() {
         </h2>
         <img src="computer.svg" className="w-64 lg:w-1/4" />
       </div>
+        <div>
+          <span className="typed text-2xl" ref={typedRef} />
+        </div>
+
     </Layout>
   );
 }
