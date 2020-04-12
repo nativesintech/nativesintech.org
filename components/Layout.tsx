df09be7d73424656728265f32c7f25d4b3579210
Adam Recvlohe <adam.recvlohe@gmail.com>
2020-04-12 14:54:27 -0500

    feat: adjust layout styles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,9 +6,7 @@ export function Layout(props: { children: React.ReactNode }) {
     <div className="flex flex-col min-h-screen">
       <Header href="" />
 
-      <main className="flex-1 w-full p-4 mx-auto md:px-8 md:py-16 bg-gray-200">
-        {props.children}
-      </main>
+      <main className="flex-1 w-full">{props.children}</main>
       <Footer />
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -34,12 +34,13 @@ import { Layout } from "../components/Layout";
 
   return (
     <Layout>
-      <div className="flex flex-col lg:flex-row items-center justify-center mx-8">
+      <div className="flex flex-col lg:flex-row items-center justify-center p-4 mx-auto md:px-8 md:py-16  bg-gray-200">
         <h2 className=" my-8 p-3 text-5xl lg:text-6xl text-gray-800 text-center leading-tight">
           Supporting software developers serving Native communities{" "}
         </h2>
         <img src="computer.svg" className="w-64 lg:w-1/4" />
       </div>
+
         <div>
           <span className="typed text-2xl" ref={typedRef} />
         </div>
