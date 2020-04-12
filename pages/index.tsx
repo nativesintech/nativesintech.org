832ea32a4cbd23a9ee8beba6814748708a81af92
Adam Recvlohe <adam.recvlohe@gmail.com>
2020-04-12 14:55:15 -0500

    feat: add more content to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,10 +41,37 @@ import { Layout } from "../components/Layout";
         <img src="computer.svg" className="w-64 lg:w-1/4" />
       </div>
 
+      <div className="px-8 py-4">
         <div>
           <span className="typed text-2xl" ref={typedRef} />
         </div>
 
+        <div className="w-1/2 text-2xl">
+          <div className="py-4">
+            Welcome 👋.{" "}
+            <span className="text-teal-500">
+              Natives in Tech is a coalition of Native and non-Native software
+              developers whose goal is to support software application
+              development that reinforces Native beliefs, knowledge, and
+              identity
+            </span>
+            . This is achieved through four initiatives: networking with
+            aspiring and experienced developers alike, creating a strong social
+            media presence on platforms familiar to developers, hosting a yearly
+            Natives in Tech conference, and building open source software that
+            Native peoples can use to cultivate healthy online communities.
+          </div>
+
+          <div>
+            Natives in Tech is a US 501(c)(3) charitable organization, funded by
+            individual donations.
+          </div>
+        </div>
+      </div>
+
+      <div className="px-8">Connect With Us!</div>
+
+      <div className="px-8">Ask a Question or Leave a Comment</div>
     </Layout>
   );
 }
