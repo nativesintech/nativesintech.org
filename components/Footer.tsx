8be5f7427797f5055ee8933d5d252edf9753d5f1
Paulie Rodriguez <paulie.j.rodriguez@gmail.com>
2020-04-17 14:57:00 -0700

    updated fill in actual SVG file to match gray-800 and teal-400, added netlify badge to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,13 @@
 export function Footer() {
   return (
-    <footer className="text-center p-10 text-gray-800">
-      Made with ❤️ by Natives in Tech
+    <footer className="p-10 text-gray-800">
+      <div className="text-center"> Made with ❤️ by Natives in Tech</div>
+      <a href="https://www.netlify.com">
+        <img
+          src="https://www.netlify.com/img/global/badges/netlify-color-bg.svg"
+          alt="Deploys by Netlify"
+        />
+      </a>
     </footer>
   );
 }
