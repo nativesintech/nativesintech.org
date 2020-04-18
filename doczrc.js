d43abb7d2017039bea50f4ba13fc17eac3a55c64
Adam Recvlohe <adam.recvlohe@gmail.com>
2020-04-18 18:25:57 -0500

    feat: setup docz

---
+++ doczrc.js
@@ -0,0 +1,8 @@
+export default {
+  title: "Natives in Tech",
+  src: ".",
+  files: "**/*.mdx",
+  typescript: true,
+  menu: ["Getting Started", { name: "Components", menu: ["Header", "Footer"] }],
+  public: "./public",
+};
---
+++ src/gatsby-theme-docz/wrapper.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+import "../../../styles/index.css";
+
+const Component = ({ children }) => <>{children}</>;
+
+export default Component;
