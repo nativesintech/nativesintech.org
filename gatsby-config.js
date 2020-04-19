c26697f56210b8c343967503bf620b45646b4840
Adam Recvlohe <adam.recvlohe@gmail.com>
2020-04-19 00:58:31 -0500

    feat: adjust build config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  plugins: ["gatsby-plugin-postcss"],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [
+          require("tailwindcss"),
+          require("postcss-preset-env"),
+          ...(process.env.NODE_ENV === "production"
+            ? [require("autoprefixer")]
+            : []),
+        ],
+      },
+    },
+  ],
 };
