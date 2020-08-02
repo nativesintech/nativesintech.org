c3e87f0cd0e9ef208d9e6207e03006de11667c0b
Adam Recvlohe <adam.recvlohe@gmail.com>
2020-08-02 18:58:17 -0500

    Add google site verification

---
+++ pages/collaborators.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+/**
+ * If we want to have a collaborators section then we can use this request
+ * to the github graphql api
+
+
+   repositories(first: 100) {
+      edges {
+        node {
+          ... on Repository {
+            collaborators {
+              edges {
+                permission
+                node {
+                  avatarUrl
+                  bio
+                  name
+                  company
+                  websiteUrl
+                  email
+                  url
+                  twitterUsername
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+  * If the collaborators permission is "MAINTAIN" then that means that they
+  * are an outside collaborator
+ */
