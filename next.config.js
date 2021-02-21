cf6d1da4f57413748f022f3d6b58abb955313076
Adam Recvlohe <adam.recvlohe@pm.me>
2021-02-21 11:51:15 -0600

    Caching assets to improve page perf

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
+const withOffline = require("next-offline");
 require("dotenv").config();
 
-module.exports = {
+const nextConfig = {
   env: {
     GITHUB_TOKEN: process.env.GITHUB_TOKEN,
   },
@@ -13,4 +14,32 @@ module.exports = {
       "/contact": { page: "/contact" },
     };
   },
+  workboxOpts: {
+    runtimeCaching: [
+      {
+        urlPattern: `/\.(?:png|jpg|jpeg|svg)$/`,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "images",
+          expiration: {
+            maxAgeSeconds: 30 * 24 * 60 * 60,
+            maxEntries: 20,
+          },
+        },
+      },
+      {
+        urlPattern: `/\.(?:ttf)$/`,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "fonts",
+          expiration: {
+            maxAgeSeconds: 60 * 60 * 24 * 365,
+            maxEntries: 20,
+          },
+        },
+      },
+    ],
+  },
 };
+
+module.exports = withOffline(nextConfig);
