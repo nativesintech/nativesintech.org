2d78313ab97797dd6745035d9f297ac5799866a8
Adam Recvlohe <adam.recvlohe@pm.me>
2021-02-21 12:41:23 -0600

    Remove runtime caching and make it compile time

--- next.config.js
+++ next.config.js
@@ -14,43 +14,6 @@ const nextConfig = {
       "/contact": { page: "/contact" },
     };
   },
-  workboxOpts: {
-    runtimeCaching: [
-      {
-        urlPattern: `/assets\/.+\.(?:png|jpg|jpeg|svg)$/`,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "assets",
-          expiration: {
-            maxAgeSeconds: 30 * 24 * 60 * 60,
-            maxEntries: 20,
-          },
-        },
-      },
-      {
-        urlPattern: `/\.(?:png|jpg|jpeg|svg)$/`,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "images",
-          expiration: {
-            maxAgeSeconds: 30 * 24 * 60 * 60,
-            maxEntries: 20,
-          },
-        },
-      },
-      {
-        urlPattern: `/fonts\/.+\.ttf$/`,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "fonts",
-          expiration: {
-            maxAgeSeconds: 60 * 60 * 24 * 365,
-            maxEntries: 20,
-          },
-        },
-      },
-    ],
-  },
 };
 
 module.exports = withOffline(nextConfig);
