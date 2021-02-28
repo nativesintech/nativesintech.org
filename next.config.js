75d61c3f36d98432c2e48a9edf3c374d659e17f1
Adam Recvlohe <adam.recvlohe@pm.me>
2021-02-27 22:57:56 -0600

    Remove runtime caching

--- next.config.js
+++ next.config.js
@@ -16,43 +16,6 @@ const nextConfig = {
       "/projects": { page: "/projects" },
     };
   },
-  workboxOpts: {
-    runtimeCaching: [
-      {
-        urlPattern: `/assets\/.+\.(?:png|jpg|jpeg|svg)$/`,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "assets",
-          expiration: {
-            maxAgeSeconds: 30 * 24 * 60 * 60,
-            maxEntries: 20,
-          },
-        },
-      },
-      {
-        urlPattern: `/\.(?:png|jpg|jpeg|svg)$/`,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "images",
-          expiration: {
-            maxAgeSeconds: 30 * 24 * 60 * 60,
-            maxEntries: 20,
-          },
-        },
-      },
-      {
-        urlPattern: `/fonts\/.+\.ttf$/`,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "fonts",
-          expiration: {
-            maxAgeSeconds: 60 * 60 * 24 * 365,
-            maxEntries: 20,
-          },
-        },
-      },
-    ],
-  },
 };
 
 module.exports = withOffline(nextConfig);
