e470d8f05570d242022086d8346138ff7b8d2b1d
Adam Recvlohe <adam.recvlohe@pm.me>
2021-06-11 14:41:47 -0500

    Fix button text

--- content/locale/en.ts
+++ content/locale/en.ts
@@ -5,7 +5,7 @@ export const en: Locale = {
     missionTitle: "Our Mission",
     missionBlurb:
       "Build a collective of Native technologists crafting free and open source technology for Native peoples.",
-    missionCTAText: "Learn More",
+    missionCTAText: "Learn More About",
     captionText: "Photo by",
     title: "Natives crafting Native-centric technology",
   },
@@ -15,13 +15,11 @@ export const en: Locale = {
     networkTitle: "Networking",
     networkBlurb: "Network with aspiring and experienced technologists alike",
     socialMediaTitle: "Social Media Presence",
-    socialMediaBlurb:
-      "Create a strong social media presence on platforms familiar to technologists",
+    socialMediaBlurb: "Create a strong social media presence on platforms familiar to technologists",
     conferenceTitle: "Yearly Conference",
     conferenceBlurb: "Host a yearly Natives in Tech conference",
     buildTitle: "Build Native-centric technology",
-    buildBlurb:
-      "Craft free and open source technology that empowers Native peoples",
+    buildBlurb: "Craft free and open source technology that empowers Native peoples",
     title: "Natives in Tech - About",
   },
   "/conference": {
@@ -30,10 +28,8 @@ export const en: Locale = {
       "The Natives in Tech Conference brings together Native technologists to share projects that empower and support Native peoples around the world. Natives in Tech Conf is completely online and free to the public.",
     conferenceForTitle: "Who is this conference for?",
     conferenceForItem1: "Natives working in technology ",
-    conferenceForItem2:
-      "Non-Natives developing technology that empowers Native peoples",
-    conferenceForItem3:
-      "Individuals and organizations seeking to engage, learn about, and support Native peoples",
+    conferenceForItem2: "Non-Natives developing technology that empowers Native peoples",
+    conferenceForItem3: "Individuals and organizations seeking to engage, learn about, and support Native peoples",
     watchTitle: "Watch Past Conferences",
     title: "Natives in Tech Conf",
   },
@@ -42,8 +38,7 @@ export const en: Locale = {
     blurb:
       "You can make a tax deductible donation through either a one-time payment or subscription to our Open Collective.",
     individualsTitle: "Individuals",
-    individualsBlurb:
-      "Become a financial contributor and help us sustain our community.",
+    individualsBlurb: "Become a financial contributor and help us sustain our community.",
     organizationsTitle: "Organizations",
     organizationsBlurb:
       "Support this project with your organization. Your logo will show up here with a link to your website.",
@@ -51,8 +46,7 @@ export const en: Locale = {
   },
   "/community": {
     h1: "Community",
-    blurb:
-      "Stay up-to-date with Natives in Tech from our various social media channels.",
+    blurb: "Stay up-to-date with Natives in Tech from our various social media channels.",
     nitCommunityTitle: "NiT Community",
     nitCommunityBlurb:
       "If you are a Native person in tech and would like to join our Native social channels then please send an email with your name, the community you would like to join, and your nation to ",
@@ -60,14 +54,12 @@ export const en: Locale = {
   },
   "/projects": {
     h1: "Projects",
-    blurb:
-      "A non-exhaustive list of projects we are working on. You can find more on our ",
+    blurb: "A non-exhaustive list of projects we are working on. You can find more on our ",
     title: "Natives in Tech - Projects",
   },
   "/contact": {
     h1: "Contact",
-    blurb:
-      "We would love to hear from you! Feel free to drop us a line below or send a message to ",
+    blurb: "We would love to hear from you! Feel free to drop us a line below or send a message to ",
     name: "Name",
     email: "Email",
     message: "Message",
@@ -84,8 +76,7 @@ export const en: Locale = {
     contact: "Contact",
   },
   footer: {
-    nonprofitBlurb:
-      "Natives in Tech is a US 501(c)(3) charitable organization, funded by individual donations.",
+    nonprofitBlurb: "Natives in Tech is a US 501(c)(3) charitable organization, funded by individual donations.",
   },
   seo: {
     title: "Natives crafting Native-centric technology",
