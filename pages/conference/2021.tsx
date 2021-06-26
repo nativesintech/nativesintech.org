a81206e8d8552a9bb910400856b0d63d76479d89
Adam Recvlohe <adam.recvlohe@pm.me>
2021-06-25 20:31:28 -0500

    Update copy for conf

--- pages/conference/2021.tsx
+++ pages/conference/2021.tsx
@@ -29,19 +29,19 @@ export default function TwentyTwentyOne() {
           </li>
           <li>
             <Anchor href="https://www.ctvnews.ca/sci-tech/complaints-flood-social-media-as-indigenous-stories-disappear-on-instagram-1.5418147">
-              Posts referencing #MMIW take down on Instagram
+              Posts referencing #MMIW taken down on Instagram
             </Anchor>
           </li>
         </ul>
         <p className="p">
-          As more of our work and livelihoods are ingrained with technology we need platforms that protect us within
+          As more of our work and livelihoods are ingrained with technology, we need platforms that protect us within
           this space. Natives in Tech Conf is an opportunity to explore solutions to these problems. Solutions for
           Native people by Native people.
         </p>
         <p className="p">
-          The conference starts on Friday Nov. 5th, 2021 with a workshop on Native language preservation. On Saturday
-          Nov. 6th, 2021 we will live-steam our virtual conference focusing on two tracks: Game Dev and Getting into
-          Tech.
+          The conference starts on Friday Nov. 5th, 2021 with a workshop on Native language preservation by{" "}
+          <Anchor href="https://indigenousinai.org/">Indigenous in AI</Anchor>. On Saturday Nov. 6th, 2021 we will
+          live-steam our virtual conference focusing on two tracks: Game Dev and Getting into Tech.
         </p>
         <p className="p">
           We hope Native people and thoughtful allies will join us for a wonderful event that uplifts Native people
