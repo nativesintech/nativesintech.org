bb75069768c07f4e447de296294ea8ab2cfbc5b3
Adam Recvlohe <adam.recvlohe@pm.me>
2022-10-18 22:13:28 -0500

    Update types defs

--- content/types.ts
+++ content/types.ts
@@ -2,3 +2,7 @@ import { en } from "./locale/en";
 import { es } from "./locale/es";
 
 export type Locale = typeof en | typeof es;
+
+export type Page = keyof Locale;
+
+export type PageKeys<Key extends Page> = keyof Locale[Key];
